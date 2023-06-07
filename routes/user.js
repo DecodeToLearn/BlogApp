2583cd9f95cb10103d6626b0ff28768e3c3e6b76
Yusuf Sengul <yusufsengul@Yusufs-iMac.local>
2023-06-07 17:07:56 +0300

    if condition commit

--- routes/user.js
+++ routes/user.js
@@ -11,21 +11,32 @@ const data = {
             baslik: "Komple Uygulamalı Web Geliştirme",
             aciklama: "Sıfırdan ileri seviyeye 'Web Geliştirme': Html, Css, Sass, Flexbox, Bootstrap, Javascript, Angular, JQuery, Asp.Net Mvc&Core Mvc",
             resim: "1.jpeg",
-            anasayfa: true
+            anasayfa: false,
+            onay: true
         },
         {
             blog_id:2,
             baslik: "Python ile Sıfırdan İleri Seviye Python Programlama",
             aciklama: "Sıfırdan İleri Seviye Python Dersleri.Veritabanı,Veri Analizi,Bot Yazımı,Web Geliştirme(Django)",
             resim: "2.jpeg",
-            anasayfa: true
+            anasayfa: false,
+            onay: true
         },
         {
             blog_id:3,
             baslik: "Sıfırdan İleri Seviye Modern Javascript Dersleri ES7+ ",
-            aciklama: "Modern javascript dersleri ile (ES6 & ES7+) Nodejs, Angular, React ve VueJs için sağlam bir temel oluşturun. ",
+            aciklama: "Modern javascript dersleri ile (ES6 & ES7+) Nodejs, Angular, React ve VueJs için sağlam bir temel oluşturun.",
             resim: "3.jpeg",
-            anasayfa: false
+            anasayfa: true,
+            onay: true
+        },
+        {
+            blog_id:4,
+            baslik: "Node.js ile Sıfırdan İleri Seviye Web Geliştirme",
+            aciklama: "Node.js ile sıfırdan ileri seviye dinamik web uygulaması geliştirmeyi öğren.",
+            resim: "4.jpeg",
+            anasayfa: true,
+            onay: true
         }
     ]
 }
