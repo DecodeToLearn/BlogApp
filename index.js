ad8d30bd05bf4cbeffed0a804b1be5d15878f6fb
Yusuf Sengul <yusufsengul@Yusufs-iMac.local>
2023-06-07 16:08:33 +0300

    initial commit

---
+++ index.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const app = express();
+const path = require("path");
+// Set View engine global variable
+app.set("view engine", "ejs");
+// Include custome Route module
+const userRoute = require("./routes/user");
+const adminRoute = require("./routes/admin");
+
+//Static Files Middleware
+app.use("/libs", express.static(path.join(__dirname, "node_modules")));
+app.use("/static", express.static(path.join(__dirname, "public")));
+
+//Router Middleware
+app.use("/admin",adminRoute);
+app.use(userRoute);
+
+/*app.listen(3000, function() {
+    console.log("listening on port 3000");
+});*/
---
+++ routes/user.js
@@ -0,0 +1,46 @@
+const express = require("express");
+const router = express.Router();
+const path = require("path");
+
+const data = {
+    title: "Popüler Kurslar",
+    categories: ["Web Geliştirme","Programlama", "Mobil Uygulamalar","Veri Analizi", "Ofis Uygulamaları"],
+    blogs:[
+        {
+            blog_id:1,
+            baslik: "Komple Uygulamalı Web Geliştirme",
+            aciklama: "Sıfırdan ileri seviyeye 'Web Geliştirme': Html, Css, Sass, Flexbox, Bootstrap, Javascript, Angular, JQuery, Asp.Net Mvc&Core Mvc",
+            resim: "1.jpeg",
+            anasayfa: true
+        },
+        {
+            blog_id:2,
+            baslik: "Python ile Sıfırdan İleri Seviye Python Programlama",
+            aciklama: "Sıfırdan İleri Seviye Python Dersleri.Veritabanı,Veri Analizi,Bot Yazımı,Web Geliştirme(Django)",
+            resim: "2.jpeg",
+            anasayfa: true
+        },
+        {
+            blog_id:3,
+            baslik: "Sıfırdan İleri Seviye Modern Javascript Dersleri ES7+ ",
+            aciklama: "Modern javascript dersleri ile (ES6 & ES7+) Nodejs, Angular, React ve VueJs için sağlam bir temel oluşturun. ",
+            resim: "3.jpeg",
+            anasayfa: false
+        }
+    ]
+}
+
+
+router.use("/blogs/:blogid", function(req, res) {
+    res.render("users/blog-details");
+});
+
+router.use("/blogs", function(req, res) {
+    res.render("users/blogs", data);
+});
+
+router.use("/", function(req, res) {
+    res.render("users/index", data);
+});
+
+module.exports = router;
